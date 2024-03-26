6ce992038c59c455754302182dbb46f3255c571d
1SkyShadow1 <Eazymoja5@gmail.com>
2024-03-26 04:35:49 -0700

    Error handling

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""
-Function top_ten
-"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Return top ten posts of subreddit
-    """
-    response = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "PythonScript"},
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code >= 300:
+    if response.status_code == 404:
         print("None")
-    else:
-        [print(child.get("data").get("title"))
-         for child in response.json().get('data').get('children')]
-
-
-if __name__ == "__main__":
-    import sys
-    top_ten(sys.argv[1])
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
